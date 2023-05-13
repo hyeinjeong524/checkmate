51a2be7b49551f1c318a917ed093cd52649f5083
hyeinjeong524 <hyeinjeong524@gmail.com>
2023-05-13 16:10:24 +0900

    Add Calendar to the main page

---
+++ client/src/Calendar.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import './Calendar.css'
+
+
+function Calendar() {
+  const daysInMonth = 31; // Total number of days in the month
+  const days = Array.from({ length: daysInMonth }, (_, index) => index + 1); // Array of day numbers
+
+  const handleDayClick = (day) => {
+    console.log(`Clicked on day ${day}`);
+    // You can perform any action here based on the clicked day
+  };
+
+  const getCalendarRows = () => {
+    const rows = [];
+    const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+
+    const weekDaysRow = weekDays.map((day, index) => (
+        <div key={index} className="calendar__day">{day}</div>
+    ));
+
+    rows.push(
+        <div key={-1} className="calendar__row">
+          {weekDaysRow}
+        </div>
+      );
+
+      
+    for (let i = 0; i < days.length; i += 7) {
+
+      const daysRow = days.slice(i, i + 7).map((day) => (
+        <button key={day} className="calendar__button" onClick={() => handleDayClick(day)}>
+          {day}
+        </button>
+      ));
+
+      rows.push(
+        <div key={i} className="calendar__row">
+          {daysRow}
+        </div>
+      );
+    
+    }
+
+    return rows;
+  };
+
+  return <div className="calendar">{getCalendarRows()}</div>;
+}
+
+export default Calendar;
