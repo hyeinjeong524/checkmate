1e6baa882d1068422a5bf811f205b7846a98ad02
hyeinjeong524 <hyeinjeong524@gmail.com>
2023-05-15 20:05:40 +0900

    Add a test box "Lists"  in 2nd column

---
+++ client/src/components/Lists.jsx
@@ -0,0 +1,25 @@
+import "./Lists.css";
+
+function Lists(){
+    return <div className="List">
+        <h1>x</h1>
+        <h1>x</h1>
+        <h1>x</h1>
+        <h1>x</h1>
+        <h1>x</h1>
+        <h1>x</h1>
+        <h1>x</h1>
+        <h1>x</h1>
+        <h1>x</h1>
+        <h1>x</h1>
+        <h1>x</h1>
+        <h1>x</h1>
+        <h1>x</h1>
+        <h1>x</h1>
+        <h1>x</h1>
+
+
+        </div>
+}
+
+export default Lists;
